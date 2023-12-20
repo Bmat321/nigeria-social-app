e728cfb496c082e64240588f15d9c9a152760c61
Bmat321 <matthewabioduntech537@gmail.com>
2023-12-20 21:27:47 +0100

    ninja social first push

--- client/ios/bmatGlobalTests/bmatGlobalTests.m
+++ client/ios/bmatGlobalTests/bmatGlobalTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BecodemyTests : XCTestCase
+@interface bmatGlobalTests : XCTestCase
 
 @end
 
-@implementation BecodemyTests
+@implementation bmatGlobalTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
